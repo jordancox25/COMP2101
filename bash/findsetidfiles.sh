fadb0bdb8b4f51ba6bd07945dc1f37fdf724aa83
Jordan Cox <jordancox25@gmail.com>
2020-01-15 14:28:29 -0800

    made script

---
+++ bash/findsetidfiles.sh
@@ -0,0 +1,9 @@
+$ ./findsetidfiles.sh
+Setuid files:
+=============
+daemon -rwsr-sr-x /usr/bin/at
+root -rwsr-sr-x /usr/lib/snapd/snap-confine
+root -rwsr-xr-x /bin/fusermount
+root -rwsr-xr-x /bin/mount
+root -rwsr-xr-x /bin/ping
+root -rwsr-xr-x /bin/su
