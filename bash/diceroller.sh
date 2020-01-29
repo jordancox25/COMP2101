a6ee1bc7e34068cb9efdd1e4e9c6031fd9243e91
Jordan Cox <jordancox25@gmail.com>
2020-01-29 12:31:28 -0800

    final

--- bash/diceroller.sh
+++ bash/diceroller.sh
@@ -6,7 +6,17 @@
 # Task: Improve this script by making it roll as many dice as there are letters in your first name, instead of just one
 
 # roll the dice and display the result
+echo "You are rolling 5 six-sided dice, and 1 twenty-sided die."
+###########################
+#Rolling the 5 6-sided dice
+#########################
 echo "
-Rolling...
-$(( RANDOM % 6 + 1)) rolled
+Rolling the 5 six-sided dice results in...
+$(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1)), $(( RANDOM % 6 + 1)) rolled "
+##########################
+#Rolling the 20-sided dice
+##########################
+echo "
+Rolling the 1 twenty-sided die results in...
+$(( RANDOM % 20 + 1)) rolled
 "
